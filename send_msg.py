a76bca5f01ab2c7100e182fbe4e7136d792df285
Tempesto <igor.it.step@gmail.com>
2019-07-11 10:03:42 +0300

    Нормализация кода

--- send_msg.py
+++ send_msg.py
@@ -20,6 +20,7 @@ def sql():
     finally:
         con.close()
 
+
 def scan_15_min():
     con = connect()
     mes_15 = []
@@ -30,19 +31,22 @@ def scan_15_min():
             for row in cursor:
                 if row['updated_at'] > time.time() - 900:
                     if row['published'] == 1 and row['recommended'] == 1:
-                        p = row['name'], float(row['price']), 'https://airsofter.world/ru-ru/product/{}'.format(row['id'])
+                        p = row['name'], float(row['price']), 'https://airsofter.world/ru-ru/product/{}'.format(
+                            row['id'])
                         mes_15.append(p)
             return mes_15
     finally:
         con.close()
 
+
 def send_message():
     msg, new_public_for_one_hour = sql()
     if not msg:
         print('Error')
-        return (False ,[])
+        return (False, [])
     else:
         return (msg, new_public_for_one_hour)
+
 
 def sender():
     condition = True
@@ -52,11 +56,12 @@ def sender():
     while condition:
         msg_15 = scan_15_min()
         if len(msg_15_prev) < len(msg_15):
-            print('dedeefefevfe',msg_15[0][0], msg_15[0][1], msg_15[0][2])
+            print('dedeefefevfe', msg_15[0][0], msg_15[0][1], msg_15[0][2])
             print(('https://airsofter.world/ru-ru/market/index'))
             Tmsg = "Name: {}, Prise: {}, {}".format(msg_15[0][0], msg_15[0][1], msg_15[0][2])
             print(Tmsg)
-            requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TG_TOKEN, CHAT_ID, Tmsg))
+            requests.get(
+                "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TG_TOKEN, CHAT_ID, Tmsg))
             msg_15_prev = msg_15
         caunter += 1
         print(caunter)
@@ -66,8 +71,10 @@ def sender():
             if len(msg_prev) < len(msg):
                 print('The site has {} new products'.format(len(new_public_for_one_hour)))
                 print(('https://airsofter.world/ru-ru/market/index'))
-                Tmsg = 'The site has {} new products'.format(len(new_public_for_one_hour))+ ' https://airsofter.world/ru-ru/market/index'
-                requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TG_TOKEN, CHAT_ID, Tmsg))
+                Tmsg = 'The site has {} new products'.format(
+                    len(new_public_for_one_hour)) + ' https://airsofter.world/ru-ru/market/index'
+                requests.get(
+                    "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TG_TOKEN, CHAT_ID, Tmsg))
                 msg_prev = msg
         if caunter == 4:
             caunter = 0

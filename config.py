b8b1a9507ecf50ed11a9f339ab5e6e23fe6954f0
Tempesto <igor.it.step@gmail.com>
2019-07-11 09:33:04 +0300

    bot

---
+++ config.py
@@ -0,0 +1,15 @@
+import pymysql.cursors
+
+TG_TOKEN = "848307854:AAElOophxSdlMf8UXubudIQnXfWv8VuU_bE"
+CHAT_ID = "249356603" #-1001192663900
+
+def connect():
+    con = pymysql.connect(
+        host = '127.0.0.1',
+        port = 3306,
+        user = 'nonroot',
+        password = 'dFsd423vcvSsxXX',
+        db = 'airsoft',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    return con
---
+++ main.py
@@ -0,0 +1,43 @@
+from telegram import Bot
+from telegram import Update
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+from send_msg import sender
+from config import TG_TOKEN
+
+def do_start(bot: Bot, update: Update):
+    print('chat_id = ', update.message.chat_id)
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        # text = update.message.chat_id,
+    )
+
+def do_echo(bot: Bot, update: Update):
+    print('chat_id = ', update.message.chat_id)
+    # text = update.message.text
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        # text=text,
+    )
+
+def main():
+    bot = Bot(token= TG_TOKEN,)
+    updater = Updater(bot=bot,)
+    # Выводит сообщение "post" при перво подлючении пользователя, запуск функции do_start
+    start_handler = CommandHandler("post", do_start)
+    message_handler = MessageHandler(Filters.text, do_echo)
+    updater.dispatcher.add_handler(start_handler)
+    updater.dispatcher.add_handler(message_handler)
+    # Запуск обновления
+    updater.start_polling()
+    # Не закрываться пока не обновится
+    updater.idle()
+    # sender()
+
+if __name__ == '__main__':
+    sender()
+    main()
+
+

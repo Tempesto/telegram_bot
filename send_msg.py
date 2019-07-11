3bc93eb5b13c09c62c573f1e5dd057ca1b551d67
Tempesto <igor.it.step@gmail.com>
2019-07-11 14:02:33 +0300

    refactoring code1

--- send_msg.py
+++ send_msg.py
@@ -17,52 +17,6 @@ def sql_query(query_str):
     finally:
         con.close()
 
-
-
-# def sql():
-#     con = connect()
-#     mes = []
-#     new_public_for_one_hour = []
-#     try:
-#         with con.cursor() as cursor:
-#             sql = 'SELECT * FROM products'
-#             cursor.execute(sql)
-#             for row in cursor:
-#                 if row['updated_at'] > time.time() - 3600:
-#                     new_public_for_one_hour.append(row)
-#                 mes.append(row)
-#             return mes, new_public_for_one_hour
-#     finally:
-#         con.close()
-#
-#
-# def scan_15_min():
-#     con = connect()
-#     mes_15 = []
-#     try:
-#         with con.cursor() as cursor:
-#             sql = 'SELECT `name`, `price`, `updated_at`, `recommended`, `published`, `id` FROM products'
-#             cursor.execute(sql)
-#             for row in cursor:
-#                 if row['updated_at'] > time.time() - 900:
-#                     if row['published'] == 1 and row['recommended'] == 1:
-#                         p = row['name'], float(row['price']), 'https://airsofter.world/ru-ru/product/{}'.format(
-#                             row['id'])
-#                         mes_15.append(p)
-#             return mes_15
-#     finally:
-#         con.close()
-
-
-# def send_message():
-#     msg, new_public_for_one_hour = sql()
-#     if not msg:
-#         print('Error')
-#         return (False, [])
-#     else:
-#         return (msg, new_public_for_one_hour)
-
-
 def sender():
     condition = True
     msg_prev = []
@@ -77,15 +31,10 @@ def sender():
 
     while condition:
         data_15 = sql_query(sql1)
-        # print(data_15)
         if data_count_15_prev < len(data_15):
-            # print('dedeefefevfe', data_15[0][0], data_15[0][1], data_15[0][2])
-            # print(('https://airsofter.world/ru-ru/market/index'))
-            # print('dwedwdwdw',data_15[0]['name'])
             Tmsg = "Name: {}, Prise: {}, https://airsofter.world/ru-ru/product/{}".format(data_15[0]['name'],
                                                                                           data_15[0]['price'],
                                                                                           data_15[0]['id'])
-            # print(Tmsg)
             requests.get(
                 "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TG_TOKEN, CHAT_ID, Tmsg))
             data_count_15_prev = len(data_15)
@@ -95,8 +44,6 @@ def sender():
         if counter == 4:
             data_60 = sql_query(sql2)
             if data_count_60_prev < len(data_60):
-                # print('The site has {} new products'.format(len(data_60)))
-                # print(('https://airsofter.world/ru-ru/market/index'))
                 Tmsg = 'The site has {} new products'.format(
                     len(data_60)) + ' https://airsofter.world/ru-ru/market/index'
                 requests.get(
